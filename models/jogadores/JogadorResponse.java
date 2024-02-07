a465dc59e451cddfee144e56dd1afc05dce7f302
Vinicius Guimarães <110570340+Sonyng@users.noreply.github.com>
2024-02-07 14:46:03 -0300

    Delete models directory

--- models/jogadores/JogadorResponse.java
+++
@@ -1,7 +0,0 @@
-package com.brasilvirtual.models.jogadores;
-
-public record JogadorResponse(Long id, String nome,String email, String dataNascimento) {
-    public JogadorResponse(Jogador jogador){
-        this(jogador.getId(), jogador.getNome(), jogador.getEmail(), jogador.getDataNascimento());
-    }
-}
